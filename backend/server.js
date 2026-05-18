c3471c0df5ed03ec6a25a9cb1186d8c728c91eef
Appu Reddy <64358995+Appureddy143@users.noreply.github.com>
2026-05-18 11:53:12 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
@@ -14,4 +15,11 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Backend running on ${PORT}`);
+});
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
